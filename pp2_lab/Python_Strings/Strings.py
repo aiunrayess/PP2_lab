da1eced306faf7c6089425557ec7c1989163901e
aiunrayess <ai_yessenbek@kbtu.kz>
2025-01-22 00:49:55 +0500

    Add files via upload

---
+++ pp2_lab/Python_Strings/Strings.py
@@ -0,0 +1,24 @@
+print("Hello")
+print('Hello')
+
+#Quotes inside Quotes
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+#Assign string to a variable
+a = "Hello"
+print(a) 
+
+#Multiline Strings
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a) 
+
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a) 
